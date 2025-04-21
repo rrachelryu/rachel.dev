bf5c53e89d1d49084007bc1616f35637f44b7a38
rachel <dev.rachelxx@gmail.com>
2025-04-21 23:31:04 +0900

    proto type

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,57 +1,39 @@
 import React from 'react'
 import { useApp } from '../context/AppContext'
-import {
-  SiReact,
-  SiSpring,
-  SiAmazonwebservices,
-  SiTypescript,
-  SiJavascript,
-  SiHtml5,
-  SiCss3,
-  SiNodedotjs,
-  SiMysql,
-  SiGithub,
-  SiThymeleaf,
-  SiSpringboot,
-  SiPostgresql,
-  SiOracle,
-  SiGit,
-  SiJquery,
-  SiVuedotjs,
-  SiEclipseide,
-} from 'react-icons/si'
 import '../assets/styles/AboutSection.css'
 
 const techStack = [
-  { icon: <SiReact />, label: 'React' },
-  { icon: <SiSpring />, label: 'Spring' },
-  { icon: <SiSpringboot />, label: 'Spring Boot' },
-  { icon: <SiThymeleaf />, label: 'Thymeleaf' },
-  { icon: <SiAmazonwebservices />, label: 'AWS' },
-  { icon: <SiJavascript />, label: 'JavaScript' },
-  { icon: <SiTypescript />, label: 'TypeScript' },
-  { icon: <SiHtml5 />, label: 'HTML5' },
-  { icon: <SiCss3 />, label: 'CSS3' },
-  { icon: <SiNodedotjs />, label: 'Node.js' },
-  { icon: <SiMysql />, label: 'MySQL' },
-  { icon: <SiPostgresql />, label: 'PostgreSQL' },
-  { icon: <SiOracle />, label: 'Oracle' },
-  { icon: <SiGithub />, label: 'GitHub' },
-  { icon: <SiGit />, label: 'Git' },
-  { icon: <SiJquery />, label: 'jQuery' },
-  { icon: <SiVuedotjs />, label: 'Vue.js' },
-  { icon: <SiEclipseide />, label: 'Eclipse' },
+  { label: 'React' },
+  { label: 'Spring' },
+  { label: 'Spring Boot' },
+  { label: 'Thymeleaf' },
+  { label: 'AWS' },
+  { label: 'JavaScript' },
+  { label: 'TypeScript' },
+  { label: 'HTML5' },
+  { label: 'CSS3' },
+  { label: 'Node.js' },
+  { label: 'MySQL' },
+  { label: 'PostgreSQL' },
+  { label: 'Oracle' },
+  { label: 'GitHub' },
+  { label: 'Git' },
+  { label: 'jQuery' },
+  { label: 'Vue.js' },
+  { label: 'Eclipse' },
 
   // 아이콘 없는 항목들 (텍스트로 대체)
-  { icon: null, label: 'JSP' },
-  { icon: null, label: 'IBatis' },
-  { icon: null, label: 'MyBatis' },
-  { icon: null, label: 'WebSquare' },
-  { icon: null, label: 'MSA' },
-  { icon: null, label: '전자정부' },
-  { icon: null, label: 'STS' },
-  { icon: null, label: 'NCRM' },
-  { icon: null, label: 'AJAX' },
+  { label: 'JSP' },
+  { label: 'IBatis' },
+  { label: 'MyBatis' },
+  { label: 'WebSquare' },
+  { label: 'IntelliJ' },
+  { label: 'DataGrip' },
+  { label: 'MSA' },
+  { label: '전자정부' },
+  { label: 'STS' },
+  { label: 'NCRM' },
+  { label: 'AJAX' },
 ]
 
 const AboutSection: React.FC = () => {
@@ -68,8 +50,7 @@ const AboutSection: React.FC = () => {
           <p>{t('aboutDesc')}</p>
           <div className="tech-stack">
             {techStack.map((tech, index) => (
-              <div key={index} className="tech-icon">
-                <div className="tech-logo">{tech.icon}</div>
+              <div key={index} className="tech-item">
                 <span>{tech.label}</span>
               </div>
             ))}
